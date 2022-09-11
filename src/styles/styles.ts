09ac9c12ad760784b09714f780d1a88b0296c37d
Anton Golubenko <anton.golubenko@gmail.com>
2022-09-11 21:08:05 +0200

    project for short front-end case.

---
+++ src/styles/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const MapsWrapper = styled.div`
+   
+    background: lightgray;
+    display: flex;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+`;
+
+
+export const ButtonStyle = styled.button`
+
+    position: absolute;
+    z-index: 3;
+    top: 600px;
+    right: 720px;
+    background-color: purple;
+    color: white;
+
+`;
+
+export const RandButton = styled.button`
+
+    position: absolute;
+    z-index: 3;
+    top: 600px;
+    left: 680px;
+    background-color: #0099cc;
+    color: white;
+
+`;
+
+
